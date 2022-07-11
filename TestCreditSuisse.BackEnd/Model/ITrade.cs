ef705f0c426c67e52e9cf7bc7379e19546e9c803
Edmilson Hora <edmilson_hora@yahoo.com.br>
2022-07-11 13:52:49 -0300

    inicial

---
+++ TestCreditSuisse.BackEnd/Model/ITrade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCreditSuisse.BackEnd.Model
+{
+    public interface ITrade
+    {
+        string ClientSector { get; set; }
+        double Value { get; set; }
+        DateTime NextPaymentDate { get; set; }
+    }
+}
---
+++ TestCreditSuisse.BackEnd/Model/Portifolio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCreditSuisse.BackEnd.Model
+{
+    public class Portifolio
+    {
+        public DateTime Date { get; set; }
+        public int NumOfTrades { get; set; }
+        public List<Trade> Trades { get; set; }
+
+        public string[] CategorizeTrades()
+        {
+            string[] result = new string[NumOfTrades];
+
+            for (int i = 0; i < NumOfTrades; i++)
+            {
+                result[i] = Trades[i].Process(Date);
+            }
+
+            return result;
+        }
+    }
+}
